0bdb34c562440aceddd25eee750729a588ede1d6
Marvin <marvinraj77@gmail.com>
2025-01-15 17:29:40 +0800

    test

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import BottomNavbar from '../components/BottomNavbar'
 
 const LandingPage = () => {
+  console.log("test");
   return (
     <main className='landing-container'>
       <h3 className='landing-welcome'>what's on your mind. anything.</h3>
